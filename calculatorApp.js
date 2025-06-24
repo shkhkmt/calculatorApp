513533a922a2244832750a64236ab7296dadb332
shkhkmt <93771404+shkhkmt@users.noreply.github.com>
2025-06-24 19:23:27 +0200

    added borders and sizes to the main container

--- calculatorApp.js
+++ calculatorApp.js
@@ -37,7 +37,35 @@ function operate(operator, a, b) {
   }
 }
 
+/*
 console.log(operate("-", 2, 3)); 
 console.log(operate("*", 2, 3));
 console.log(operate("+", 2, 3)); 
 console.log(operate("/", 2, 3)); 
+*/ 
+
+// UI 
+
+const linkElement = document.createElement('link'); 
+linkElement.rel = 'stylesheet'; 
+linkElement.href = 'styles.css'; 
+document.head.appendChild(linkElement); 
+
+let content = document.querySelector('#content'); 
+
+let calculator = document.querySelector('.calculator'); 
+calculator.style.width = '600px'; 
+calculator.style.height = '900px'; 
+calculator.style.border = '2px solid black'; 
+calculator.style.display = 'flex'; 
+calculator.style.flexDirection = 'column'; 
+
+let screen = document.querySelector('#screen'); 
+screen.style.width = '520px'; 
+screen.style.height = '150px'; 
+screen.style.border = '2px solid black'; 
+
+let buttons = document.querySelector('#buttons'); 
+buttons.style.width = '520px'; 
+buttons.style.height = '590px'; 
+buttons.style.border = '2px solid black'; 
