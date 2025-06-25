c21ea0655721f4e6a4b5ce43a33a8e883f02aa4e
shkhkmt <93771404+shkhkmt@users.noreply.github.com>
2025-06-25 22:37:37 +0200

    added rounded edges and background to the UI.

--- calculatorApp.js
+++ calculatorApp.js
@@ -51,7 +51,11 @@ linkElement.rel = 'stylesheet';
 linkElement.href = 'styles.css'; 
 document.head.appendChild(linkElement); 
 
+let body = document.querySelector('body'); 
+
 let content = document.querySelector('#content'); 
+content.style.alignItems = 'anchor-center'; 
+content.style.width = '-webkit-fill-available-'; 
 
 let calculator = document.querySelector('.calculator'); 
 calculator.style.width = '600px'; 
@@ -59,13 +63,20 @@ calculator.style.height = '900px';
 calculator.style.border = '2px solid black'; 
 calculator.style.display = 'flex'; 
 calculator.style.flexDirection = 'column'; 
+calculator.style.justifyContent = 'center'; 
 
 let screen = document.querySelector('#screen'); 
 screen.style.width = '520px'; 
 screen.style.height = '150px'; 
+screen.style.display = 'flex'; 
+screen.style.alignSelf = 'stretch'; 
+screen.style.margin = '2em auto'; 
 screen.style.border = '2px solid black'; 
 
 let buttons = document.querySelector('#buttons'); 
 buttons.style.width = '520px'; 
 buttons.style.height = '590px'; 
+buttons.style.display = 'flex'; 
+buttons.style.alignSelf = 'stretch'; 
+buttons.style.margin = '2em auto'; 
 buttons.style.border = '2px solid black'; 
